7a21e9cb37b87a4080660b47c93b8fba43c02683
cmanish049 <cmanish049@gmail.com>
2020-08-11 13:46:21 +0545

    react router

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
     },
     devtool: 'eval-cheap-module-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true // to support client side routing
     }
 };
